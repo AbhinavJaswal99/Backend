95e8160c6da29a5705d9ed6e368f77c1013862f7
Your Name <you@example.com>
2019-04-26 20:07:36 +0530

    xcxc

---
+++ WebAPIMake/Controllers/OrderItemController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIMake.Models;
+
+namespace WebAPIMake.Controllers
+{
+    public class OrderItemController : ApiController
+    {
+       abhinav_testEntities2 db = new abhinav_testEntities2();
+
+        [HttpGet]
+        public IEnumerable<OrderItem> GetOrderItems()
+        {
+           return  db.OrderItems.ToList();
+        }
+
+        [HttpGet]
+        public OrderItem GetOrderItem(int id)
+        {
+            var getOrderIt = db.OrderItems.SingleOrDefault(x => x.Id == id);
+
+            if(getOrderIt == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return getOrderIt;
+        }
+
+        [HttpPost]
+        public OrderItem CreateOrderItem(OrderItem orderit)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+                db.OrderItems.Add(orderit);
+                db.SaveChanges();
+
+                return orderit;
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        public void UpOrderItem(int id, OrderItem orderitem)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var Orderss = db.OrderItems.SingleOrDefault(x => x.Id == id);
+            if(Orderss == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            orderitem.OrderId = Orderss.OrderId;
+            orderitem.ItemId = Orderss.ItemId;
+            orderitem.ItemQuantity = Orderss.ItemQuantity;
+            orderitem.ItemPrice = Orderss.ItemPrice;
+            orderitem.ItemTotal = Orderss.ItemTotal;
+
+            db.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteOrderItem(int id)
+        {
+            var orderitem = db.OrderItems.SingleOrDefault(x => x.Id == id);
+            if(orderitem == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            db.OrderItems.Remove(orderitem);
+            db.SaveChanges();
+
+
+        }
+    }
+}
---
+++ WebAPIMake/Controllers/OrderdbController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIMake.Models;
+
+namespace WebAPIMake.Controllers
+{
+    public class OrderdbController : ApiController
+    {
+        abhinav_testEntities2 db = new abhinav_testEntities2();
+        
+        [HttpGet]
+        public IEnumerable<Orderdb> GetOrderdbs()
+        {
+            return  db.Orderdbs.ToList();
+           //var hello = db.Orderdbs.Include("tblCustomer").ToList();
+           // return hello;
+        }
+
+        [HttpGet]
+        public Orderdb GetOrderdb(int id)
+        {
+            var getord = db.Orderdbs.SingleOrDefault(x => x.Id == id);
+
+            if(getord == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return getord;
+        }
+
+        [HttpPost]
+        public Orderdb CreateOrderdb(Orderdb orderdb)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+                db.Orderdbs.Add(orderdb);
+                db.SaveChanges();
+
+                return orderdb;
+            }
+        
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        public void UpdateOrderdb(int id, Orderdb orderdb)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var orderss = db.Orderdbs.SingleOrDefault(x => x.Id == id);
+
+            if(orderss == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            orderdb.OrderNo = orderss.OrderNo;
+            orderdb.TotalPrice = orderss.TotalPrice;
+            orderdb.TotalQuantity = orderss.TotalQuantity;
+            orderdb.OrderDate = orderss.OrderDate;
+
+            db.SaveChanges();
+        }
+
+        public void DeleteOrderdb(int id)
+        {
+            var orderss = db.Orderdbs.SingleOrDefault(x => x.Id == id);
+
+            if(orderss == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            db.Orderdbs.Remove(orderss);
+            db.SaveChanges();
+        }
+
+
+    }
+}
